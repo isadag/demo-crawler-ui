f435152a91c352bf3ab893c8d3b4422dc2f6af50
gitName <a@b.c>
2020-09-05 14:47:41 +0200

    Fetching and rendering screenshot - quick and dirty

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
+  // const statusApiUrl = "https://demo-crawler-api.herokuapp.com/api/crawlerstatus";
+  const screenshotApiUrl = "https://localhost:5001/api/pagecrawler";
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [status, setStatus] = useState(null);
 
+  // useEffect(() => {
+  //   fetch(statusApiUrl)
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setStatus(result.status);
+  //       },
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     )
+  // }, [])
+
   useEffect(() => {
-    fetch("https://demo-crawler-api.herokuapp.com/api/crawlerstatus")
+    fetch(screenshotApiUrl)
       .then(res => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
-          setStatus(result.status);
+          setStatus(result);
+          console.log(result);
         },
         (error) => {
           setIsLoaded(true);
           setError(error);
+          console.log(error);
         }
       )
   }, [])
@@ -26,9 +46,12 @@ function App() {
     return <div>Loading...</div>;
   } else {
     return (
-      <h1>
-        The status is: {status}
-      </h1>
+      <React.Fragment>
+        <h1>Web page screenshot</h1>
+        <div className="imageContainer">
+          <img src={`data:image/png;base64,${status}`} alt="screenshot" />
+        </div>
+      </React.Fragment>
     );
   }
 }
