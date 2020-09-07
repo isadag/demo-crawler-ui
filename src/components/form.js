1a4953f5f33123620dc560f92b832e0c710ff567
gitName <a@b.c>
2020-09-07 16:24:56 +0200

    Remove current autofocus functionality, disturbing on mobile.

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,7 @@ const Form = (props) => {
     return (
         <React.Fragment>
             <form onSubmit={props.onSubmit} className={props.className}>
-                <input type="text" name="url" placeholder="http://www.google.com" onChange={props.onChange} autoFocus />
+                <input type="text" name="url" placeholder="http://www.google.com" onChange={props.onChange} />
                 <input type="submit" value="Get result" />
             </form>
         </React.Fragment>
