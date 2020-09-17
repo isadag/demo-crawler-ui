41b102dbfa93819b949824f3503729fc43d9dc1d
gitName <a@b.c>
2020-09-17 16:05:23 +0200

    Fixed API url

--- src/components/pageResultWrapper.js
+++ src/components/pageResultWrapper.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
 
 function PageResultWrapper() {
     const classes = useStyles();
-    const pageCrawlerApiUrl = "https://demo-crawler-api.herokuapp.com/api/pagecrawler";
+    const pageCrawlerApiUrl = "https://demo-crawler-api.herokuapp.com/api/page-crawler";
     // const pageCrawlerApiUrl = "https://localhost:5001/api/page-crawler";
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
